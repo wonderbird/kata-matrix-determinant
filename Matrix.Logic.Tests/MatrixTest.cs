81d1eceb8d66cea01e315cae3fb88d17021fda39
Stefan Boos <stefan.boos@gmx.de>
2021-03-15 05:43:08 +0100

    test: add 5x5 and 10x10 matrix test

--- Matrix.Logic.Tests/MatrixTest.cs
+++ Matrix.Logic.Tests/MatrixTest.cs
@@ -53,6 +53,57 @@ namespace Matrix.Logic.Tests
             RunTest(expected, testData);
         }
 
+        // https://matrix.reshish.com/de/detCalculation.php
+        [Fact]
+        public static void Determinant_5x5Matrix_ReturnsCorrectDeterminant()
+        {
+            var testData = new[]
+            {
+                new[]
+                {
+                    new[] { 1, 3, 4, 2, 7 },
+                    new[] { 2, 8, 4, 3, 6 },
+                    new[] { 1, 6, 4, 0, 3 },
+                    new[] { 7, 0, 5, 2, 4 },
+                    new[] { 13, 2, 8, 7, 0 },
+                }
+            };
+            var expected = new[]
+            {
+                3313
+            };
+
+            RunTest(expected, testData);
+        }
+
+        // https://matrix.reshish.com/de/detCalculation.php
+        [Fact]
+        public static void Determinant_10x10Matrix_ReturnsCorrectDeterminant()
+        {
+            var testData = new[]
+            {
+                new[]
+                {
+                    new[] { 3, 9, 7, 8, 3, 4, 9, 8, 5, 7 },
+                    new[] { 3, 4, 9, 5, 8, 7, 4, 3, 9, 5 },
+                    new[] { 8, 3, 2, 7, 4, 3, 8, 5, 7, 9 },
+                    new[] { 3, 4, 5, 7, 4, 3, 8, 5, 7, 3 },
+                    new[] { 5, 7, 8, 4, 5, 7, 9, 3, 7, 9 },
+                    new[] { 8, 6, 9, 2, 8, 0, 3, 4, 9, 8 },
+                    new[] { 5, 4, 7, 9, 6, 9, 3, 2, 3, 2 },
+                    new[] { 8, 9, 6, 7, 6, 1, 7, 8, 2, 3 },
+                    new[] { 4, 5, 2, 1, 3, 4, 5, 6, 3, 5 },
+                    new[] { 0, 1, 3, 8, 5, 1, 0, 3, 4, 2 },
+                }
+            };
+            var expected = new[]
+            {
+                11746830
+            };
+
+            RunTest(expected, testData);
+        }
+
         private static void RunTest(int[] expected, int[][][] testData)
         {
             for (var index = 0; index < expected.Length; index++)
